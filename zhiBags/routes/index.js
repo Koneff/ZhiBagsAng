d7418b08617fc0e24367e17ce444a85c33799ee8
Koneff <a.konefff@gmail.com>
2015-07-28 02:01:02 +0300

    GET all bags (first express route)

--- zhiBags/routes/index.js
+++ zhiBags/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+
+
+var Bag = mongoose.model('Bag');
+var Quote = mongoose.model('Quote');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/bags', function(req, res, next) {
+    Bag.find(function(err, bags){
+        if(err){ return next(err); }
+
+        res.json(bags);
+    });
+});
+
 
 module.exports = router;
